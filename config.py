e94eb013c05d20ddce48ee7f2a4b54ce1c5cc813
nandobfer <nandobfer@yahoo.com.br>
2022-03-25 06:52:43 -0300

    Added variable number of enemies

--- config.py
+++ config.py
@@ -11,7 +11,12 @@ bulletImg = pygame.image.load('bullet.png')
 bulletSpeed = 1
 
 # Enemy starting variables
-enemyPos_x = random.randint(0, 730)
-enemyPos_y = random.randint(30, 60)
-enemyImg = pygame.image.load('enemy.png')
+enemiesQuantity = 6
 enemySpeed = 0.15
+enemyPos_x = []
+enemyPos_y = []
+enemyImg = []
+for i in range(enemiesQuantity):
+    enemyImg.append(pygame.image.load('enemy.png'))
+    enemyPos_x.append(random.randint(0, 730))
+    enemyPos_y.append(random.randint(30, 60))
--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import pygame, config, math
+import pygame, config, math, random
 from init import initializeGame, initializeScreen, initializePlayer
 
 # Calling initializing functions
@@ -18,31 +18,22 @@ bulletState = 'ready'
 shooting_x = 0
 # Initializing enemy variables
 enemySpeed = config.enemySpeed
-enemyPos_x = config.enemyPos_x
-enemyPos_y = config.enemyPos_y
-newEnemyPos_x, newEnemyPos_y = enemySpeed, 0
-
-# def enemyMove(enemy_x, enemy_y, player_x, player_y):
-#     if player_x - enemy_x > 0:
-#         x = enemySpeed
-#     elif player_x - enemy_x < 0:
-#         x = -enemySpeed
-#     else:
-#         x = 0
-#     if player_y - enemy_y > 0:
-#         y = enemySpeed
-#     elif player_y - enemy_y < 0:
-#         y = -enemySpeed
-#     else:
-#         y = 0
-#     return x, y
+enemyPos_x = []
+enemyPos_y = []
+newEnemyPos_x = []
+newEnemyPos_y = []
+for i in range(config.enemiesQuantity):
+    enemyPos_x.append(config.enemyPos_x[i])
+    enemyPos_y.append(config.enemyPos_y[i])
+    newEnemyPos_x.append(enemySpeed)
+    newEnemyPos_y.append(0)
 
 def player(x,y):
     # screen.blit(Object, Position): draw Object into the screen at Position
     screen.blit(config.playerImg, (x,y))
 
-def enemy(x,y):
-    screen.blit(config.enemyImg, (x, y))
+def enemy(x,y,i):
+    screen.blit(config.enemyImg[i], (x, y))
 
 def shoot(x, y):
     global bulletState
@@ -105,34 +96,36 @@ while running:
 
 
     # Enemy movement events
-    # newEnemyPos_x, newEnemyPos_y = enemyMove(enemyPos_x, enemyPos_y, playerPos_x, playerPos_y)
+    for i in range(config.enemiesQuantity):
+        enemyPos_x[i] += newEnemyPos_x[i]
+        if enemyPos_x[i] <= 10:
+            newEnemyPos_x[i] = -newEnemyPos_x[i]
+            enemyPos_y[i] += 40
+        elif enemyPos_x[i] >= 730:
+            newEnemyPos_x[i] = -newEnemyPos_x[i]
+            enemyPos_y[i] += 40
+
+        # Collision event
+        if getCollision(enemyPos_x[i], enemyPos_y[i], shooting_x, bulletPos_y):
+            resetBullet()
+            score += 1
+            print(score)
+            enemyPos_x[i] = random.randint(0, 730)
+            enemyPos_y[i] = random.randint(30, 60)
+
+        enemy(enemyPos_x[i], enemyPos_y[i], i)
 
     # Setting new position
     playerPos_x += newPlayerPos_x
     playerPos_y += newPlayerPos_y
-    enemyPos_x += newEnemyPos_x
-    # enemyPos_y += newEnemyPos_y
 
-    # Setting boundaries
+    # Setting player boundaries
     if playerPos_x <= 10:
         playerPos_x = 10
     elif playerPos_x >= 730:
         playerPos_x = 730
-    if enemyPos_x <= 10:
-        enemyPos_x = 10
-        newEnemyPos_x = -newEnemyPos_x
-        enemyPos_y += 40
-    elif enemyPos_x >= 730:
-        enemyPos_x = 730
-        newEnemyPos_x = -newEnemyPos_x
-        enemyPos_y += 40
 
-    # Collision event
-    if getCollision(enemyPos_x, enemyPos_y, shooting_x, bulletPos_y):
-        resetBullet()
-        score += 1
-        print(score)
+
 
     player(playerPos_x,playerPos_y)
-    enemy(enemyPos_x,enemyPos_y)
     pygame.display.update()
