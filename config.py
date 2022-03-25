c97c57d12f80756ddd43c631e8fe51feff3559f9
nandobfer <nandobfer@yahoo.com.br>
2022-03-25 03:47:45 -0300

    Added starting variables to config.py

---
+++ config.py
@@ -0,0 +1,11 @@
+# Player starting variables
+playerPos_x = 370
+playerPos_y = 480
+playerImg = pygame.image.load('player.png')
+playerSpeed = 0.2
+
+# Enemy starting variables
+enemyPos_x = 370
+enemyPos_y = 80
+enemyImg = pygame.image.load('enemy.png')
+enemySpeed = 0.15
