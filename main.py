b4dcec476f7bc4a08bc8cbe32252cb4643f6288e
nandobfer <nandobfer@yahoo.com.br>
2022-03-25 01:58:43 -0300

    Added title and icon

--- main.py
+++ main.py
@@ -6,6 +6,11 @@ pygame.init()
 # create a screen
 screen = pygame.display.set_mode((800,600))
 
+# Title and Icon
+pygame.display.set_caption("Space Invaders")
+icon = pygame.image.load('spaceship.png')
+pygame.display.set_icon(icon)
+
 # Game loop
 running = True
 while running:
