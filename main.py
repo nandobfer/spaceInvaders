07351b64039f1241983645f12ff923001f71a8c1
nandobfer <nandobfer@yahoo.com.br>
2022-03-25 02:55:25 -0300

    Moved inicializing methods to another file

--- main.py
+++ main.py
@@ -1,21 +1,8 @@
-import pygame
+from init import *
+inicializeGame()
+playerImg, playerPos_x, playerPos_y = inicializePlayer()
+screen = inicializeScreen()
 
-# Inicialize pygame
-pygame.init()
-
-# create a screen (width,height)
-screen = pygame.display.set_mode((800,600))
-
-# Title and Icon
-pygame.display.set_caption("Space Invaders")
-icon = pygame.image.load('spaceship.png')
-pygame.display.set_icon(icon)
-
-# Player
-playerImg = pygame.image.load('player.png')
-
-playerPos_x = 370
-playerPos_y = 480
 
 def player(x,y):
     # screen.blit(Object, Position): draw Object into the screen at Position
