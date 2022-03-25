825e6a5d8d77908d2a72bffc83c2a8b204b83ffe
nandobfer <nandobfer@yahoo.com.br>
2022-03-25 04:21:04 -0300

    Enemy spawns randomly inside a range

--- config.py
+++ config.py
@@ -7,7 +7,7 @@ playerImg = pygame.image.load('player.png')
 playerSpeed = 0.2
 
 # Enemy starting variables
-enemyPos_x = 370
-enemyPos_y =
+enemyPos_x = random.randint(0, 730)
+enemyPos_y = random.randint(30, 60)
 enemyImg = pygame.image.load('enemy.png')
-enemySpeed = 0.15
+enemySpeed = 0.05
