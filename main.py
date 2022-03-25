d87402fd920bf36e529444688a99d9f21310cbe5
nandobfer <nandobfer@yahoo.com.br>
2022-03-25 02:18:34 -0300

    Created player() function

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import pygame
 # Inicialize pygame
 pygame.init()
 
-# create a screen
+# create a screen (width,height)
 screen = pygame.display.set_mode((800,600))
 
 # Title and Icon
@@ -11,9 +11,25 @@ pygame.display.set_caption("Space Invaders")
 icon = pygame.image.load('spaceship.png')
 pygame.display.set_icon(icon)
 
+# Player
+playerImg = pygame.image.load('player.png')
+playerX = 370
+playerY = 480
+
+def player():
+    # screen.blit(Object, Position): draw Object into the screen at Position
+    screen.blit(playerImg, (playerX,playerY))
+
 # Game loop
 running = True
 while running:
+    # RGB - Red, Green, Blue
+    screen.fill((0, 0, 255))
+
+    # Close window event
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+
+    player()
+    pygame.display.update()
